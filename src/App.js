b3c36f26bbf20f358660db7cf9618d9078947dab
Jacob Stanger <jstangwork@gmail.com>
2020-06-23 09:14:23 -0600

    adding react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TuneSpot</h1>
+        <Form>
+          <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Control type="text" placeholder="Song Title / Artist" />
+            <Button variant="primary" type="submit">
+              Search
+            </Button>
+          </Form.Group>
+        </Form>
       </header>
     </div>
   );
